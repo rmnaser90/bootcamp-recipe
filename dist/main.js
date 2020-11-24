c47494325b9bde6ecc3ab89cd841b8dde481b2de
rmnaser90 <rmnaser90@gmail.com>
2020-11-24 11:04:13 +0200

    initializing repo

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+const renderer = new Renderer
+const apiManager = new APImanagerFront
+
+const standby = function(obj) {
+    console.log('error');
+    $(obj).attr('src','default.png') 
+    $(obj).css('width','50%')
+}
+
+
+
